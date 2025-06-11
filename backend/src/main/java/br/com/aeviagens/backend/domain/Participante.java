709a2aa50ee3f69f5cccfef5369edef9d5abe210
Israel da Rosa Barcelos <israelbarcelos6@gmail.com>
2025-06-11 03:59:05 -0300

    Ajuste no modelo de Participante de uma viagem

--- backend/src/main/java/br/com/aeviagens/backend/domain/Participante.java
+++ backend/src/main/java/br/com/aeviagens/backend/domain/Participante.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 public class Participante {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String CPF;
